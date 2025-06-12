d43d605cdeb93aa757c4874bfa7caf63b63781b2
snoopysecurity <sams@snyk.io>
2025-06-12 23:52:23 +0100

    new blog

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250527.213413';
+const cacheName = 'chirpy-20250613.065057';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
