4223d96565945eef9debe81cb1fb9d599696218e
Sam Sanoop <samsanoop@outlook.com>
2026-03-28 16:23:22 +0000

    new blog

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250613.065057';
+const cacheName = 'chirpy-20260329.002248';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
